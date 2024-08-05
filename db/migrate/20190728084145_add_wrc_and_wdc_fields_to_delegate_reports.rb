c6776d88989efd1d5be334ef4e372e38182519fd
lnzainn <99952754+lnzainn@users.noreply.github.com>
2024-08-05 20:14:01 +0530

    Update 20190728084145_add_wrc_and_wdc_fields_to_delegate_reports.rb

--- db/migrate/20190728084145_add_wrc_and_wdc_fields_to_delegate_reports.rb
+++ db/migrate/20190728084145_add_wrc_and_wdc_fields_to_delegate_reports.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
-class AddWrcAndWdcFieldsToDelegateReports < ActiveRecord::Migration[5.2]
+# class AddWrcAndWdcFieldsToDelegateReports < ActiveRecord::Migration[5.2]
+#   def change
+#     add_column :delegate_reports, :wrc_feedback_requested, :boolean, null: false, default: false
+#     add_column :delegate_reports, :wrc_incidents, :string, default: nil
+#     add_column :delegate_reports, :wdc_feedback_requested, :boolean, null: false, default: false
+#     add_column :delegate_reports, :wdc_incidents, :string, default: nil
+#   end
+# end
+
+class AddWrcAndWicFieldsToDelegateReports < ActiveRecord::Migration[5.2]
   def change
     add_column :delegate_reports, :wrc_feedback_requested, :boolean, null: false, default: false
     add_column :delegate_reports, :wrc_incidents, :string, default: nil
-    add_column :delegate_reports, :wdc_feedback_requested, :boolean, null: false, default: false
-    add_column :delegate_reports, :wdc_incidents, :string, default: nil
+    add_column :delegate_reports, :wic_feedback_requested, :boolean, null: false, default: false
+    add_column :delegate_reports, :wic_incidents, :string, default: nil
   end
 end
